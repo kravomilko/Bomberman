44608d3752fd3b7667edf8a2eab5d7d6b5af6d5d
kravomilko <f.janikovsky@gmail.com>
2018-06-16 17:41:06 +0200

    Kato je Momot

--- js/playground.js
+++ js/playground.js
@@ -1,6 +1,27 @@
+//define global variables
 var bombs = [];
 var idBombCounter = 0;
 var timeClock = 20;
+
+var heightDevider = 15;
+var playgroundHeight = parseInt(window.screen.height * 0.9 - (window.screen.height * 0.9) % heightDevider);
+var block = playgroundHeight / heightDevider;
+
+var widthDevider = 20;
+var playgroundWidth = block * widthDevider;
+
+var playgroundMap = new Array(heightDevider);
+for (var m = 0; m < heightDevider; m++) {
+  playgroundMap[m] = new Array(widthDevider);
+}
+
+
+//playground.js implementation
+function createPlayground() {
+    myGameArea.start();
+    fico = new createPlayer("./sources/players/fico.png", 80, 80);
+    createWalls();
+}
 
 function removeFlames(_flames, _myInterval) {
     for(var k = 0; k < _flames.length; k++) {
@@ -19,17 +40,19 @@ function expandFlame(posX, posY, _power, _bomb) {
 }
 
 function createPlane(posX, posY, _power, _flames) {
-    var power = _power * 0.06;
-	var moves = [[(posX - (window.screen.height * (0.03))), (posY - (window.screen.width * (0.09 + power)))],
-				[(posX - (window.screen.height * (0.09 + power))), (posY - (window.screen.width * (0.03)))],
-				[(posX + (window.screen.height * (0.03 + power))), (posY - (window.screen.width * (0.03)))],
-                [(posX - (window.screen.height * (0.03))), (posY + (window.screen.width * (0.03 + power)))]];
+    var power = (_power+1) * block;
+
+    var moves = [[posX - power, posY],
+				[posX + power, posY],
+				[posX, posY - power],
+                [posX, posY + power]];
+
 	for(var j = 0; j < 4; j++) {
 		var flame = document.createElement('img');
 		flame.style.position = 'fixed';
 		flame.src = "./sources/elements/explosion.png";
-		flame.style.width = '6%'
-		flame.style.height = '6%';
+		flame.style.width = block + 'px'
+		flame.style.height = block + 'px';
 		var rect = flame.getBoundingClientRect();
 		flame.style.top = moves[j][0] + 'px';
 		flame.style.left = moves[j][1] + 'px';
@@ -38,22 +61,17 @@ function createPlane(posX, posY, _power, _flames) {
 	}
 }
 
-function createPlayground() {
-    myGameArea.start();
-    fico = new createPlayer("./sources/players/fico.png", 10, 10);
-}
-
 function createBomb(posX, posY) {
     var counter = 3500; // 5s - time to bomb explosion
     var bomb = document.createElement('img');
     bomb.id = 'bomb' + (++idBombCounter);
     bomb.style.position = 'fixed';
     bomb.src = "./sources/elements/bomb.png";
-    bomb.style.width = '6%'
-    bomb.style.height = '6%';
+    bomb.style.width = block + 'px'
+    bomb.style.height = block + 'px';
     var rect = bomb.getBoundingClientRect();
-    bomb.style.top = (posX - window.screen.height * 0.03) + 'px';
-    bomb.style.left = (posY - window.screen.width * 0.03) + 'px';
+    bomb.style.top = parseInt(posX/block) * block + 'px';
+    bomb.style.left = parseInt(posY/block) * block + 'px';
     document.body.appendChild(bomb);
 
     var bomb_array = [bomb.id, counter];
@@ -66,24 +84,28 @@ function createPlayer(img_src, posX, posY) {
     player.src = img_src;
     player.style.top = posX + 'px';
     player.style.left = posY + 'px';
-    player.style.width = '10%'
-    player.style.height = '10%';
+    player.style.width = block + 'px';
+    player.style.height = block + 'px';
     document.body.appendChild(player);
 
     var _posX = posX;
     var _posY = posY;
 
     this.move = function(xMove, yMove) {
-        _posX += xMove;
-        _posY += yMove;
-        player.style.top = _posX + 'px'; 
-        player.style.left = _posY + 'px'; 
+        var i = parseInt((_posX + xMove)/block);
+        var j = parseInt((_posY + yMove)/block);
+        if(playgroundMap[i][j] != "f") {
+            _posX += xMove;
+            _posY += yMove;
+            player.style.top = _posX + 'px'; 
+            player.style.left = _posY + 'px'; 
+        }
     }
 
     this.launch = function() {
         var rect = player.getBoundingClientRect();
-        var cy = rect.left + rect.width * 0.5;    // find center of first image
-        var cx = rect.top + rect.height * 0.5;
+        var cx = rect.top + rect.height * 0.5;    // find center of first image
+        var cy = rect.left + rect.width * 0.5;
         new createBomb(cx, cy);
     }
 }
@@ -109,8 +131,8 @@ function updateGameArea() {
 			var bomb = document.getElementById(bombs[i][0]);
             bomb.src = "./sources/elements/explosion.png";
             var _rect = bomb.getBoundingClientRect();
-            var _cy = _rect.left + _rect.width * 0.5;    // find center of first image
-            var _cx = _rect.top + _rect.height * 0.5;
+            var _cy = _rect.left;// + _rect.width * 0.5;    // find center of first image
+            var _cx = _rect.top;// + _rect.height * 0.5;
             expandFlame(_cx, _cy, 3, bomb);
 		}
 		else if(bombs[i][1] <= 0) {
@@ -118,6 +140,41 @@ function updateGameArea() {
             i--;
 		}
 	}
+}
+
+function drawPlayground(x, y) {
+    var wall = document.createElement('img');
+    wall.style.position = 'absolute';
+    wall.src = "./sources/walls/wall1.png";
+    wall.style.top = x * block + 'px';
+    wall.style.left = y * block + 'px';
+    wall.style.width = block + 'px';
+    wall.style.height = block + 'px';
+    document.body.appendChild(wall);
+}
+
+function createWalls() {
+    for(var i = 0; i < heightDevider; i++) {
+        for(var j = 0; j < widthDevider; j++) {
+            playgroundMap[i][j] = 0;
+        }
+    }
+    for(i = 0; i < heightDevider; i++) {
+        drawPlayground(i, 0);
+        playgroundMap[i][0] = "f";
+    }
+    for(i = 1; i < widthDevider; i++) {
+        drawPlayground(0, i);
+        playgroundMap[0][i] = "f";
+    }
+    for(i = 0; i < heightDevider; i++) {
+        drawPlayground(i, widthDevider-1);
+        playgroundMap[i][widthDevider-1] = "f";
+    }
+    for(i = 1; i < widthDevider; i++) {
+        drawPlayground(heightDevider-1, i);
+        playgroundMap[heightDevider-1][i] = "f";
+    }
 }
 
 var myGameArea = {
